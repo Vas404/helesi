8c83fa3c31e1dee76a38907fade2cfc8ac1294f1
Andreas Hadjigavriel <info@newcotech.io>
2022-06-14 15:33:52 +0300

    Revert "Helesi Commit Final"
    This reverts commit 464e363b3c7f721b8f139fb60d71cc77d601ac84.

--- app/routes/errors.js
+++
@@ -1,9 +0,0 @@
-var router = require('express').Router()
-var path = require('path')
-
-router.get('/403', (req, res, next)=>{
-    console.log('unauthorized')
-    res.sendFile('/unauthorized.html', { root: 'app/static_error'})
-})
-
-module.exports = router
